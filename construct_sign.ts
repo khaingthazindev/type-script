135690c5e568e994fabe3dd5faab4a1378ff7c8a
khaingthazindev <khaingthazin.dev@gmail.com>
2026-05-11 10:27:48 +0630

    Constructor signatures useful for passing class as argu and dependency injection

--- construct_sign.ts
+++ construct_sign.ts
@@ -9,3 +9,13 @@ class Human extends Mamal {
 		console.log('Human ', name);
 	}
 }
+type Con = {
+	new (name:string):Mamal
+}
+let myClass:Con = Human;
+let human = new myClass('KTZ');
+console.log('human ', human);
+
+myClass = Mamal;
+let mamal = new myClass('Mamal1');
+console.log('mamal ', mamal);
